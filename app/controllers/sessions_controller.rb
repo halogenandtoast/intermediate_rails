b5c5fc092b86033b84f29fb9cc5e282a839505de
halogenandtoast <halogenandtoast@gmail.com>
2015-03-09 10:47:46 -0400

    Initial Commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+    user = authenticate_session(session_params, email_or_username: %i(email username))
+
+    if sign_in(user)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email_or_username, :password)
+  end
+end
+
