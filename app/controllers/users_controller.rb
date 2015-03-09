f596ee3d4c7bbb4d4baa21dc9e25177baa4e112f
halogenandtoast <halogenandtoast@gmail.com>
2015-03-09 13:48:07 -0400

    Add user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
 
+  def show
+    @user = User.find(params[:id])
+    @shouts = @user.shouts
+  end
+
   def new
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   resources :shouts, only: [:create]
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 end
